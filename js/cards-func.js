f6feba6511831e098161249e068804895914cd46
Carol Fantinate <velvet.visage2026@gmail.com>
2026-08-10 17:51:54 -0300

    Mais seções

---
+++ js/cards-func.js
@@ -0,0 +1,80 @@
+const funcionalidades = [
+    {
+        icon: "assets/icons/map-pin.png",
+        title: "Beauty Scan",
+        description: "Escaneie seu rosto com nossa tecnologia de IA avançada."
+    },
+
+    {
+        icon: "assets/icons/color-analysis.png",
+        title: "Color Analysis",
+        description: "Descubra sua cartela de cores ideal e saiba quais tons realçam sua beleza."
+    },
+
+    {
+        icon: "assets/icons/skin-analysis.png",
+        title: "Skin Analysis",
+        description: "Identifique as  características da sua pele e receba recomendações personalizadas."
+    },
+    {
+        icon: "assets/icons/map-pin.png",
+        title: "Sugestões de Cabelo",
+        description: "Cores e estilos de cabelo que harmonizam com seu tom de pele e estilo."
+    },
+
+    {
+        icon: "assets/icons/color-analysis.png",
+        title: "Assistente IA",
+        description: "Tire dúvidas e receba orientações personalizadas com nossa assistente IA."
+    },
+
+    {
+        icon: "assets/icons/skin-analysis.png",
+        title: "Profissionais Próximos (a)",
+        description: "Identifique as  características da sua pele e receba recomendações personalizadas."
+    }, 
+    
+    {
+        icon: "assets/icons/map-pin.png",
+        title: "Recomendações de Maquiagem ",
+        description: "Sugestões de maquiagens que combinam com você e sua paleta de cores."
+    },
+
+    {
+        icon: "assets/icons/color-analysis.png",
+        title: "Conteúdo Exclusivo",
+        description: "Sugestões de maquiagens que combinam com você e sua paleta de cores."
+    },
+
+    {
+        icon: "assets/icons/skin-analysis.png",
+        title: "Histórico de Análises",
+        description: "Acompanhe sua evolução e veja suas análises sempre que quiser."
+    },
+];
+
+const containerCards = document.querySelector(".container-cards");
+
+funcionalidades.forEach((funcionalidade) => {
+
+    const card = document.createElement("div");
+    card.classList.add("card-func");
+
+    card.innerHTML = `
+                <img 
+                    src="${funcionalidade.icon}" 
+                    alt="${funcionalidade.title}" 
+                    class="card-icon"
+                >
+
+                <h4 class="card-title">
+                    ${funcionalidade.title}
+                </h4>
+
+                <p class="card-description">
+                    ${funcionalidade.description}
+                </p>
+            `;
+
+    containerCards.appendChild(card);
+});
