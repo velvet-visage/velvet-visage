e5bb6dac3898d6f31a33302dd73f651e9e549cc5
Carol Fantinate <velvet.visage2026@gmail.com>
2026-08-14 19:25:09 -0300

    ajustes e melhorias

--- js/card-valores.js
+++ js/card-valores.js
@@ -1,22 +1,22 @@
 const valores = [
     {
-        icon: "assets/icons/map-pin.png",
+        icon: "assets/icons/valores/light.png",
         title: "Inovação",
         description: "Usamos tecnologia de ponta para entregar resultados precisos."
     },
 
     {
-        icon: "assets/icons/map-pin.png",
+        icon: "assets/icons/valores/paintbrush.png",
         title: "Personalização",
         description: "Cada pessoa é única e nossas recomendações também."
     },
     {
-        icon: "assets/icons/map-pin.png",
+        icon: "assets/icons/valores/protect.png",
         title: "Confiança",
         description: "Prioridade e segurança são prioridade em tudo que fazemos."
     },
     {
-        icon: "assets/icons/map-pin.png",
+        icon: "assets/icons/valores/smartphone-love.png",
         title: "Acolhimento",
         description: "Estamos aqui para te apoiar em cada etapa da sua jornada."
     },
@@ -30,10 +30,12 @@ valores.forEach((valor) => {
     card.classList.add("card-valores");
 
     card.innerHTML = `
-        <img 
-            src="${valor.icon}" 
-            alt="${valor.title}" 
-        >
+        <div class="icon-background">
+            <img 
+                src="${valor.icon}" 
+                alt="${valor.title}" 
+            >
+        </div>
         <h4>${valor.title}</h4>
         <p>${valor.description}</p>
     `;
--- js/cards-tech.js
+++ js/cards-tech.js
@@ -1,21 +1,21 @@
 const tecnologias = [
     {
-        icon: "assets/icons/map-pin.png",
+        icon: "assets/icons/tecnologias/sparkles.png",
         title: "Personalização Inteligente",
         description: "Cada recomendação é adaptada às suas características, proporcionando uma experiência exclusiva e alinhada às suas necessidades."
     },
     {
-        icon: "assets/icons/map-pin.png",
+        icon: "assets/icons/tecnologias/smartphone-security.png",
         title: "Segurança e Privacidade",
         description: "Seus dados e análises são protegidos para garantir uma experiência confiável e preservar a sua privacidade."
     },
     {
-        icon: "assets/icons/tecnologias/alvo.png",
+        icon: "assets/icons/tecnologias/goal.png",
         title: "Precisão em cada detalhe",
         description: "Nossa tecnologia realiza análises detalhadas que oferecem recomendações mais assertivas, auxiliando você em escolhas com mais confiança."
     },
     {
-        icon: "assets/icons/tecnologias/celular.png",
+        icon: "assets/icons/tecnologias/smartphone-hand.png",
         title: "Praticidade para o dia-a-dia",
         description: "Acompanhe sua jornada de beleza com praticidade em um aplicativo completo e intuitivo."
     },
